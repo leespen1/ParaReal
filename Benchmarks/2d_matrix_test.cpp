29f45f559694dba1c8888fa1003cfbb1ce860256
Spencer Lee <leespen1@msu.edu>
2021-12-11 13:44:22 -0500

    Added support for matrices

---
+++ Benchmarks/2d_matrix_test.cpp
@@ -0,0 +1,114 @@
+#include<mpi.h>
+#include "/home/spencer/GradSchool/ParallelComputing/FinalProject/ParaReal/Src/parareal.hpp"
+#include "/home/spencer/GradSchool/ParallelComputing/FinalProject/ParaReal/Src/solvers.hpp"
+#include "/home/spencer/GradSchool/ParallelComputing/FinalProject/ParaReal/Src/helpers.hpp"
+#include "/home/spencer/GradSchool/ParallelComputing/FinalProject/ParaReal/Src/pointnd.hpp"
+#include<iostream>
+using std::cout; using std::endl;
+
+#include<algorithm>
+#include<iterator>
+using std::fill_n;
+using std::copy;
+using std::begin; using std::end;
+
+#define ROOT 0
+
+
+
+template <typename T>
+T f_dahlquist(T y, double t) {
+    double mat_init[2][2] = {1.0, 0.0, 0.0, 1.0};
+    matrixND<2,2> m = matrixND<2,2>(mat_init);
+    //double lambda = -1.0;
+    return m*y;
+}
+
+
+int main(int argc, char **argv) {
+    cout.precision(15); // Doubles have 15-digit precision
+
+    MPI_Init(NULL,NULL);
+
+    int my_rank, num_ranks;
+    MPI_Comm_size(MPI_COMM_WORLD,&num_ranks);
+    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+
+    //if (my_rank == ROOT)
+    //    cout << endl;
+
+    //int num_fine_solves = 1000/num_ranks;
+    int num_fine_solves = 50/num_ranks;
+    const int N = 2;
+    double init_pt_data[N] = {1.0, 2.0};
+    pointND<N> init_pt = pointND<N>(init_pt_data);
+
+    MPI_Datatype MPI_POINTND;
+    make_pointND_MPI_Datatype(N, &MPI_POINTND);
+
+    parareal_prob<pointND<N>> prob = parareal_prob<pointND<N>>(
+        0.0, 1.0, init_pt,
+        norm_pointND<N>,
+        MPI_POINTND
+    );
+
+    parareal_sol<pointND<N>> dummy_sol; // Just 
+
+    #define NUM_RUNS 100
+    int num_points = dummy_sol.num_points;
+
+    double parareal_sol_durations_avg[num_points];
+    fill_n(parareal_sol_durations_avg, num_points, 0.0);
+    double serial_sol_duration_avg = 0;
+
+    parareal_sol<pointND<N>> sol;
+    parareal_sol<pointND<N>> serial_sol;
+
+    for (int i=0; i < NUM_RUNS; ++i) {
+
+        solve_parareal(
+            prob, sol, 0,
+            [=](pointND<N> y, double t1, double t2) { // Coarse Solver, few timesteps
+                return exp_euler<pointND<N>>(y, t1, t2, f_dahlquist<pointND<N>>, 1);
+            },
+            [=](pointND<N> y, double t1, double t2) { // Fine Solver, more timesteps
+                return exp_euler<pointND<N>>(y, t1, t2, f_dahlquist<pointND<N>>, num_fine_solves);
+            }
+        );
+
+        solve_parareal_serial(
+            prob, serial_sol,
+            [=](pointND<N> y, double t1, double t2) { // Fine Solver, more timesteps
+                return exp_euler<pointND<N>>(y, t1, t2, f_dahlquist<pointND<N>>, num_fine_solves);
+            }
+        );
+
+        // The following solution memory is only allocated for root
+        if (my_rank == ROOT) {
+            for (int j=0; j < num_points; ++j)
+                parareal_sol_durations_avg[j] += sol.sol_durations[j];
+            serial_sol_duration_avg += serial_sol.sol_durations[0];
+        }
+    }
+
+    // Take average of times, overwrite them on the solutions (for display)
+    if (my_rank == ROOT) {
+        for (int j=0; j < num_points; ++j) {
+            //parareal_sol_durations_avg[j] /= NUM_RUNS;
+            sol.sol_durations[j] = parareal_sol_durations_avg[j];
+        }
+        //serial_sol_duration_avg /= NUM_RUNS;
+        serial_sol.sol_durations[0] = serial_sol_duration_avg;
+
+        // Display Results
+        cout << "ParaReal" << endl;
+        display_solution_csv_2(sol); // Apparently don't have to provide template arguments if compiler can deduce type
+        cout << "Serial" << endl;
+        display_solution_csv_2(serial_sol);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
+
+
--- Src/pointnd.hpp
+++ Src/pointnd.hpp
@@ -9,7 +9,7 @@ using std::pow;
 
 template <int N>
 struct pointND {
-    int size = N;
+    const int size = N;
     double data[N];
 
     pointND() {};
@@ -27,10 +27,12 @@ struct pointND {
 
     pointND operator+(const pointND& pt) {
         this->check_size_equal(pt);
-        double *new_data = new double[this->size];
-        pointND new_pointND = pointND<N>(new_data);
+        //double *new_data = new double[this->size];
+        //pointND new_pointND = pointND<N>(new_data);
+        pointND<N> new_pointND;
         for (int i = 0; i < new_pointND.size; ++i)
             new_pointND.data[i] = (*this)(i) + pt(i);
+        //delete new_data;
         return new_pointND;
     }
 
@@ -102,4 +104,98 @@ std::ostream &operator<<(std::ostream &os, const pointND<N>& pt) {
     return os;
 }
 
+template <int N1, int N2>
+struct matrixND {
+    const int size = N1*N2;
+    const int num_rows = N1;
+    const int num_cols = N2;
+    double data[N1][N2];
+
+
+    matrixND(double init_data[N1][N2]) {
+        for (int i = 0; i < this->num_rows; ++i)
+            for (int j = 0; j < this->num_rows; ++j)
+                data[i][j] = init_data[i][j];
+    }
+
+    pointND<N1> operator*(const pointND<N2>& pt) {
+        //if (this->num_cols != pt.size)
+        //    throw "Matrix*Vector dimensions do not match";
+        pointND<N1> new_pointND;
+        int i;
+        int j;
+        double pt_entry;
+        for (i = 0; i < this->num_rows; ++i) {
+            pt_entry = 0;
+            for (j=0; j < this->num_cols; ++j)
+                pt_entry += (*this)(i, j) * pt(j);
+            new_pointND.data[i] = pt_entry;
+        }
+        return new_pointND;
+    }
+
+    matrixND& operator=(const matrixND& mat) {
+        this->check_size_equal(mat);
+        for (int i = 0; i < this->size; ++i)
+            this->data[i] = mat.data[i];
+        return *this;
+    }
+
+    matrixND operator+(const matrixND& mat) {
+        this->check_size_equal(mat);
+        matrixND<N1, N2> new_matrixND;
+        for (int i = 0; i < new_matrixND.size; ++i)
+            new_matrixND.data[i] = (*this)(i) + mat(i);
+        return new_matrixND;
+    }
+
+    matrixND operator-(const matrixND& mat) {
+        this->check_size_equal(mat);
+        matrixND<N1, N2> new_matrixND;
+        for (int i = 0; i < new_matrixND.size; ++i)
+            new_matrixND.data[i] = (*this)(i) - mat(i);
+        return new_matrixND;
+    }
+
+    matrixND operator*(const double& scalar) {
+        matrixND<N1, N2> new_matrixND;
+        for (int i = 0; i < new_matrixND.size; ++i)
+            new_matrixND.data[i] = (*this)(i)*scalar;
+        return new_matrixND;
+    }
+
+    double& operator()(int i, int j) const {
+        if (i > this->num_rows || i < 0)
+            throw "Row index for matrixND is out of bounds";
+        if (j > this->num_cols || j < 0)
+            throw "Col index for matrixND is out of bounds";
+        return this->data[i][j];
+    }
+
+    double& operator()(int i, int j) {
+        if (i > this->num_rows || i < 0)
+            throw "Row index for matrixND is out of bounds";
+        if (j > this->num_cols || j < 0)
+            throw "Col index for matrixND is out of bounds";
+        return this->data[i][j];
+    }
+
+    double operator()(int i) const {
+        if (i > this->size || i < 0)
+            throw "Index for pointND is out of bounds";
+        return this->data[i];
+    }
+
+    double& operator()(int i) {
+        if (i > this->size || i < 0)
+            throw "Index for pointND is out of bounds";
+        return this->data[i];
+    }
+
+    void check_size_equal(const matrixND& mat) {
+        if (this->size != mat.size)
+            throw "Tried assignment between points of unequal size";
+    }
+};
 #endif
+
